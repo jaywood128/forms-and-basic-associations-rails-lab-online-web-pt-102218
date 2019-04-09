86b6bb59cb0e448ae5dc69c619c39fdf3ed385fe
jaywood128 <jaywood128@gmail.com>
2019-04-09 16:45:46 -0700

    Done.

--- app/models/song.rb
+++ app/models/song.rb
@@ -1,3 +1,21 @@
+require 'pry'
 class Song < ActiveRecord::Base
+  belongs_to :artist
+  belongs_to :genre 
+  has_many :notes
+  def artist_name=(name)
+    self.artist = Artist.find_or_create_by(name: name)
+  end
+  def artist_name 
+    self.artist ? self.artist.name : nil
+  end
+
+  def genre_id=(id)
+    self.genre = Genre.find_or_create_by(id: id)
+  end
+
+  def genre_id
+    self.genre.id ? self.artist.genre.id : nil
+  end
   # add associations here
 end
